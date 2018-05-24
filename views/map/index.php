e786e1148b0f720248404768d4b230c678043378
Jason Weidner <jasonweidner@Jasons-MacBook-Pro.local>
2018-05-24 15:46:16 +1000

    Added google map

--- views/map/index.php
+++ views/map/index.php
@@ -34,4 +34,43 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
-</div>
+
+
+<!-- 
+<!DOCTYPE html>
+<html>
+  <head> -->
+<!--     <title>Simple Map</title>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">    
+  </head> -->
+<!--   <body> -->
+
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+
+    <div id="map"></div>
+    <script>
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -27.4954258, lng: 153.0098414},
+          zoom: 12
+        });
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCHxf6ELukFVk-CCXMUSeA1A7xCV64qm4k&callback=initMap"
+    async defer></script>
+<!--   </body>
+</html> -->
