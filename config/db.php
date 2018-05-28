dd656e9668029a2fb901e7f250f1895b6e662b12
Jason Weidner <jasonweidner@Jasons-MacBook-Pro.local>
2018-05-29 09:44:57 +1000

    Configuring cleardb connection.

--- config/db.php
+++ config/db.php
@@ -2,10 +2,28 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=uqst_db',
-    'username' => 'root',
-    'password' => 'Reallysimple2',
+    // 'dsn' => 'mysql:host=localhost;dbname=uqst_db',
+    // 'username' => 'root',
+    // 'password' => 'Reallysimple2',
     'charset' => 'utf8',
+       'dsn'   => '',
+    'hostname' => $cleardb_server,
+    'username' => $cleardb_username,
+    'password' => $cleardb_password,
+    'database' => $cleardb_db,
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
@@ -13,3 +31,8 @@ return [
     //'schemaCache' => 'cache',
 
 ];
+
+
+<?php
+
+   
