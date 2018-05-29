c8fbb113a340a0c0a55b46f919be3f4838b54ec8
Jason Weidner <jasonweidner@Jasons-MacBook-Pro.local>
2018-05-29 11:59:50 +1000

    configure db.php for production

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=uqst_db',
+    'dsn' => '', //mysql:host=localhost;dbname=uqst_db',
     'username' => 'root',
     'password' => 'Reallysimple2',
     'charset' => 'utf8',
