6a04b522a8758cef70054903ceef496c9f7c4da6
Jason Weidner <jasonweidner@Jasons-MacBook-Pro.local>
2018-05-29 10:12:31 +1000

    Adjusting DEV environment.

--- config/web.php
+++ config/web.php
@@ -85,7 +85,7 @@ if (YII_ENV_DEV) {
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['127.0.0.1', '::1', '130.102.13.4'],
     ];
 
     $config['bootstrap'][] = 'gii';
